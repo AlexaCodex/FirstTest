fda7ef0454b90ec207b7313b58e956a964c6ae64
Rajshall <rajshall@yahoo.com>
2017-10-30 21:04:43 +0000

    intial commit

---
+++ SampleTest/TestClass.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTest
+{
+    [TestFixture]
+    [Parallelizable]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod3()
+        {
+            Console.WriteLine("first passing test");
+        }
+
+        [SetUp]
+        public void OpenBrowser()
+        {
+            Console.WriteLine("opening browser");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("Closing browser");
+        }
+    }
+}
---
+++ SampleTest/TestClass2.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTest
+{
+    [TestFixture]
+    [Parallelizable]
+   // [Ignore("skip the test")]
+    public class TestClass2
+    {
+        [Test]
+        public void TestMethod2()
+        {
+            Console.WriteLine("test method");
+        }
+
+        [Test]
+        public void xyz()
+        {
+            Assert.Ignore("skip the test");
+            Console.WriteLine("print xyz");
+        }
+    }
+}
